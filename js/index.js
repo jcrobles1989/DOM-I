cc9437961168e66d310eaec2dc73078da0fc1887
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-04-07 00:21:50 -0700

    re-attempted project finished

--- js/index.js
+++ js/index.js
@@ -43,11 +43,9 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 // Nav starts here
 
-document.querySelector('a:first-child').textContent = 'Services'
+document.querySelector('nav a:first-child').textContent = siteContent['nav']['nav-item-1']
 
-document.querySelector('a:first-child').textContent = siteContent['nav']['nav-item-1']
-
-document.querySelector('nav a:nth-child(2)').textContent =  siteContent['nav']['nav-item-2']
+document.querySelector('nav a:nth-child(2)').textContent = siteContent['nav']['nav-item-2']
 
 document.querySelector('nav a:nth-child(3)').textContent = siteContent['nav']['nav-item-3']
 
@@ -55,81 +53,75 @@ document.querySelector('nav a:nth-child(4)').textContent = siteContent['nav']['n
 
 document.querySelector('nav a:nth-child(5)').textContent = siteContent['nav']['nav-item-5']
 
-document.querySelector('a:last-child').textContent = siteContent['nav']['nav-item-6']
+document.querySelector('nav a:last-child').textContent = siteContent['nav']['nav-item-6']
 
-const newHome = document.createElement('a');
+const parentElement = document.querySelector('nav')
 
-const newFAQ = document.createElement('a')
+const newHomeLink = document.createElement('a')
+newHomeLink.textContent = 'Home'
 
-newHome.textContent = 'Home'
+const newFAQLink = document.createElement('a')
+newFAQLink.textContent = 'FAQ'
 
-newFAQ.textContent = 'FAQ';
+parentElement.prepend(newHomeLink)
+parentElement.appendChild(newFAQLink)
 
-const parentElement = document.querySelector('nav');
-
-parentElement.prepend(newHome)
-
-parentElement.appendChild(newFAQ);
-
-const navColor = document.getElementsByTagName('a')
-
-Array.from(navColor).forEach(element => {
-  element.style.color = 'green';
-})
+const navColor = document.querySelectorAll('a')
+navColor.forEach( item => {item.style.color = 'green'})
 
 // This closes the nav 
 
 // CTA begins here 
 
-document.querySelector('h1').textContent = siteContent['cta']['h1']
+document.querySelector('h1').textContent = siteContent['cta']['h1'];
 
-document.querySelector('button').textContent = siteContent['cta']['button']
+document.querySelector('button').textContent = siteContent['cta']['button'];
 
-document.getElementById('cta-img').src = siteContent['cta']['img-src']
+document.querySelector('#cta-img').setAttribute('src', siteContent['cta']['img-src'])
 
 // This closes CTA 
 
 // Main-content begins here 
 
-document.getElementsByTagName('h4')[0].textContent = siteContent['main-content']['features-h4']
+document.querySelectorAll('h4')[0].textContent = siteContent['main-content']['features-h4']
 
-document.getElementsByTagName('p')[0].textContent = siteContent['main-content']['features-content']
+document.querySelectorAll('p')[0].textContent = siteContent['main-content']['features-content']
 
-document.getElementsByTagName('h4')[1].textContent = siteContent['main-content']['about-h4']
+document.querySelectorAll('h4')[1].textContent = siteContent['main-content']['about-h4']
 
-document.getElementsByTagName('p')[1].textContent = siteContent['main-content']['about-content']
+document.querySelectorAll('p')[1].textContent = siteContent['main-content']['about-content']
 
-document.getElementsByTagName('h4')[2].textContent = siteContent['main-content']['services-h4']
+document.querySelector('.middle-img').setAttribute('src', siteContent['main-content']['middle-img-src'])
 
-document.getElementsByTagName('p')[2].textContent = siteContent['main-content']['services-content']
+document.querySelectorAll('h4')[2].textContent = siteContent['main-content']['services-h4']
 
-document.getElementsByTagName('h4')[3].textContent = siteContent['main-content']['product-h4']
+document.querySelectorAll('p')[2].textContent = siteContent['main-content']['services-content']
 
-document.getElementsByTagName('p')[3].textContent = siteContent['main-content']['product-content']
+document.querySelectorAll('h4')[3].textContent = siteContent['main-content']['product-h4']
 
-document.getElementsByTagName('h4')[4].textContent = siteContent['main-content']['vision-h4']
+document.querySelectorAll('p')[3].textContent = siteContent['main-content']['product-content']
 
-document.getElementsByTagName('p')[4].textContent = siteContent['main-content']['vision-content']
+document.querySelectorAll('h4')[4].textContent = siteContent['main-content']['vision-h4']
 
-document.getElementById('middle-img').src = siteContent['main-content']['middle-img-src']
+document.querySelectorAll('p')[4].textContent = siteContent['main-content']['vision-content']
 
 // This closes main-content
 
 // Contact section begins here
 
-document.getElementsByTagName('h4')[5].textContent = siteContent['contact']['contact-h4']
+document.querySelectorAll('h4')[5].textContent = siteContent['contact']['contact-h4']
 
-document.getElementsByTagName('p')[5].textContent = siteContent['contact']['address']
+document.querySelectorAll('p')[5].textContent = siteContent['contact']['address']
 
-document.getElementsByTagName('p')[6].textContent = siteContent['contact']['phone']
+document.querySelectorAll('p')[6].textContent = siteContent['contact']['phone']
 
-document.getElementsByTagName('p')[7].textContent = siteContent['contact']['email']
+document.querySelectorAll('p')[7].textContent = siteContent['contact']['email']
 
 // This closes contact section
 
 // Footer begins here 
 
-document.getElementsByTagName('p')[8].textContent = siteContent['footer']['copyright']
+document.querySelectorAll('p')[8].textContent = siteContent['footer']['copyright']
 
 // This closes the footer
 
